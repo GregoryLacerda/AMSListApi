a1b417d240e8f92b6c86a1b245f81e079afb8408
Gregory Lacerda Batista <gregorylacerda@hotmail.com>
2022-05-26 01:27:47 -0300

    Created class DevConfiguration to tests

---
+++ src/main/java/com/gregory/AMSList/config/DevConfig.java
@@ -0,0 +1,23 @@
+package com.gregory.AMSList.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.gregory.AMSList.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instantiateDB() {
+		
+		this.dbService.instantiate();
+	}
+	
+}
